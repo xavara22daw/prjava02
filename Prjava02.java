f9e962cfb9c2b09311b554864b9efb3118007901
xavara22daw <xavara22daw@proton.me>
2022-12-19 20:48:25 +0100

    Versió 1.2 del projecte prjava02

--- Prjava02.java
+++ Prjava02.java
@@ -27,6 +27,7 @@ public class Prjava02 {
             System.out.println("Sistema operatiu: " + System.getProperty("os.name"));
             System.out.println("Versió OS: " + System.getProperty("os.version"));
             System.out.println("Creació d'una branca del projecte prjava02");
+            System.out.println("Afegint més codi des de la branca00 del projecte prjava02");
         }
         catch (IOException e) {
 		System.out.println("Exception occurred");
